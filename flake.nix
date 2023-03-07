4e0c1c2d2c36161400bd067dbb2e19df3ba36f95
Litchi Pi <litchi.pi@proton.me>
2023-03-07 10:28:10 +0100

    Initial commit
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import inputs.nixpkgs { inherit system; };
+    lib = pkgs.lib;
+
+    python_version = pkgs.python310;
+    python_packages_version = pkgs.python310Packages;
+    pythonpkg = python_version.withPackages (p: with p; [
+    ]);
+
+    repl_script = pkgs.writeShellScript "json_repl" "${pythonpkg}/bin/python3 ./json_repl.py $@";
+  in {
+    # TODO    Add tool to overlay
+    # overlays.default = prev: final: {};
+    apps.default = { type = "app"; program = "${repl_script}"; };
+    devShells.default = pkgs.mkShell {
+      buildInputs = [
+        pythonpkg
+      ];
+      PYTHONPATH = "${pythonpkg}/${pythonpkg.sitePackages}:$PYTHONPATH";
+      LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib$LD_LIBRARY_PATH";
+    };
+  });
+}
---
+++ json_repl.py
@@ -0,0 +1,176 @@
+# Taken from https://coderwall.com/p/w78iva/give-your-python-program-a-shell-with-the-cmd-module
+
+import os
+import json
+import base64
+import argparse
+from cmd import Cmd
+
+DEFAULT_FORMAT = "text"
+DATA_LOADERS = {
+    "text": lambda x: x,
+    "int": lambda x: int(x),
+    "float": lambda x: float(x),
+    "json": lambda x: json.loads(x),
+}
+
+FILE_LOADERS = {
+    "text": ("r", lambda f: f.read()),
+    "json": ("r", lambda f: json.load(f)),
+    "binary": ("rb", lambda f: base64.b64encode(f.read()).decode()),
+}
+
+# TODO      Tab completion
+#       On keys
+#       On files to write
+#       On formats
+class JsonREPL(Cmd):
+    def __init__(self, dataf):
+        Cmd.__init__(self)
+        self.dataf = dataf
+        if os.path.isfile(dataf):
+            with open(dataf, "r") as f:
+                self.inner_data = json.load(f)
+        else:
+            self.inner_data = dict()
+
+    def save(self):
+        with open(self.dataf, "w") as f:
+            json.dump(self.inner_data, f)
+
+    def unfold_key(self, keys, data=None, set_to=None, rm=False):
+        if data is None:
+            data = dict.copy(self.inner_data)
+
+        if len(keys) == 0:
+            if set_to:
+                return set_to
+            elif rm:
+                return {}
+            else:
+                return data
+        elif keys[0] not in data.keys():
+            if set_to:
+                new_data = dict()
+            elif rm:
+                return {}
+            else:
+                return None
+        elif type(data[keys[0]]) == dict:
+            new_data = dict.copy(data[keys[0]])
+        elif set_to:
+            new_data = dict()
+        else:
+            return data[keys[0]]
+
+        data[keys[0]] = self.unfold_key(keys[1:], data=new_data, set_to=set_to, rm=rm)
+        return data
+
+    def update_inner_data(self, key, data, rm=False):
+        if "." not in key:
+            if rm and key in self.inner_data.keys():
+                del self.inner_data[key]
+            else:
+                self.inner_data[key] = data
+        else:
+            self.inner_data.update(
+                self.unfold_key(key.split("."), set_to=data, rm=rm)    # type: ignore [no-redef]
+            )
+        self.save()
+
+    def do_add(self, args):
+        ''' Usage: add <key> <data> [format=fmt]
+        '''
+        argl = args.split()
+        if len(args) < 2:
+            self.do_help("add")
+            return
+        key = argl[0]
+        data = args.removeprefix(key).lstrip()
+        if "format=" in data.split()[-1]:
+            fmt = data.split()[-1].split("=")[1]
+            data = data[:-1]
+        else:
+            fmt = DEFAULT_FORMAT
+        if fmt not in DATA_LOADERS.keys():
+            print("Format unknwon")
+            print("Available data formats: {}".format(
+                ", ".join([
+                    l + (" (default)"*(l==DEFAULT_FORMAT)) for l in DATA_LOADERS.keys()
+                ]))
+            )
+            return
+        try:
+            data = DATA_LOADERS[fmt](data)
+        except Exception as err:
+            print(f"Loading the data failed: {err}")
+            return
+        self.update_inner_data(key, data)
+
+    def do_get(self, args):
+        ''' Usage: get <key>
+        '''
+        args = args.split()
+        if len(args) < 1:
+            print(json.dumps(self.inner_data, indent=2))
+            return
+        key = args[0]
+        d = self.unfold_key(key.split("."))
+        if not d:
+            print(f"Key {key} doesn't point to any data yet")
+            return
+        if type(d) == dict:
+            print(json.dumps(d, indent=2))
+        else:
+            print(d, f"({type(d)})")
+
+    def do_rm(self, args):
+        ''' Usage: rm <key>
+        '''
+        args = args.split()
+        if len(args) < 1:
+            self.do_help("rm")
+            return
+        key = args[0]
+        self.update_inner_data(key, None, rm=True)
+
+    def do_from_file(self, args):
+        ''' Usage: from_file <key> <file path> <format>
+            Format can be: text, binary, json
+        '''
+        args = args.split()
+        if len(args) < 3:
+            self.do_help("from_file")
+            return
+        key = args[0]
+        fpath = args[1]
+        fmt = args[2]
+        if fmt not in FILE_LOADERS.keys():
+            print(f"Wrong format {fmt}")
+            print("Available file formats: {}".format(", ".join(list(FILE_LOADERS.keys()))))
+            return
+        if not os.path.isfile(fpath):
+            print(f"{fpath}: No such file or directory")
+            return
+        try:
+            with open(fpath, FILE_LOADERS[fmt][0]) as f:
+                data = FILE_LOADERS[fmt][1](f)
+        except Exception as err:
+            print(f"Error while loading from file: {err}")
+            return
+        self.update_inner_data(key, data)
+
+    def do_EOF(self, args):
+        print("")
+        raise SystemExit
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("json_file", help="JSON file to save the data to")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    prompt = JsonREPL(args.json_file)
+    prompt.prompt = '>>> '
+    prompt.cmdloop("")
